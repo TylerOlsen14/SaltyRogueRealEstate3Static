520fa3e0568deab119bbd7733f2be73e9729a952
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-07-18 12:46:13 -0600

    added data for Emma Street, carousel not working yet

---
+++ src/components/EmmaStreetImages.js
@@ -0,0 +1,97 @@
+const EmmaStreetImages = [
+  // "../assets/EmmaStreet/EmmaStreet00002.webp",
+  // "../assets/EmmaStreet/EmmaStreet00003.webp",
+  // "../assets/EmmaStreet/EmmaStreet00004.webp",
+  // "../assets/EmmaStreet/EmmaStreet00005.webp",
+  // "../assets/EmmaStreet/EmmaStreet00006.webp",
+  // "../assets/EmmaStreet/EmmaStreet00007.webp",
+  // "../assets/EmmaStreet/EmmaStreet00008.webp",
+  // "../assets/EmmaStreet/EmmaStreet00009.webp",
+  // "../assets/EmmaStreet/EmmaStreet00010.webp",
+  // "../assets/EmmaStreet/EmmaStreet00011.webp",
+  // "../assets/EmmaStreet/EmmaStreet00012.webp",
+  // "../assets/EmmaStreet/EmmaStreet00013.webp",
+  // "../assets/EmmaStreet/EmmaStreet00014.webp",
+  // "../assets/EmmaStreet/EmmaStreet00015.webp",
+  // "../assets/EmmaStreet/EmmaStreet00016.webp",
+  // "../assets/EmmaStreet/EmmaStreet00017.webp",
+  // "../assets/EmmaStreet/EmmaStreet00018.webp",
+  // "../assets/EmmaStreet/EmmaStreet00019.webp",
+  // "../assets/EmmaStreet/EmmaStreet00020.webp",
+  // "../assets/EmmaStreet/EmmaStreet00021.webp",
+  // "../assets/EmmaStreet/EmmaStreet00022.webp",
+  // "../assets/EmmaStreet/EmmaStreet00023.webp",
+  // "../assets/EmmaStreet/EmmaStreet00024.webp",
+  // "../assets/EmmaStreet/EmmaStreet00025.webp",
+  // "../assets/EmmaStreet/EmmaStreet00026.webp",
+  // "../assets/EmmaStreet/EmmaStreet00027.webp",
+  // "../assets/EmmaStreet/EmmaStreet00028.webp",
+  // "../assets/EmmaStreet/EmmaStreet00029.webp",
+  // "../assets/EmmaStreet/EmmaStreet00030.webp",
+  // "../assets/EmmaStreet/EmmaStreet00031.webp",
+  // "../assets/EmmaStreet/EmmaStreet00032.webp",
+  // "../assets/EmmaStreet/EmmaStreet00033.webp",
+  // "../assets/EmmaStreet/EmmaStreet00034.webp",
+  // "../assets/EmmaStreet/EmmaStreet00035.webp",
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/ISuk7070zpttd61000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00001.webp',
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/IS6ajn81lduzt61000000000.webp',
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/ISukrbxrxea0971000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00003.webp',
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/ISa17zvw3retz51000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00004.webp',
+    altText: 'Slide 4',
+    caption: 'Slide 4'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/ISmepzelt623971000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00005.webp',
+    altText: 'Slide 5',
+    caption: 'Slide 5'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/IS2v4ndqzi6wz51000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00006.webp',
+    altText: 'Slide 6',
+    caption: 'Slide 6'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/ISe8nnwepyt5971000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00007.webp',
+    altText: 'Slide 7',
+    caption: 'Slide 7'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/ISuo2bvjvayyz51000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00008.webp',
+    altText: 'Slide 8',
+    caption: 'Slide 8'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/IS62lbe8lql8971000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00009.webp',
+    altText: 'Slide 9',
+    caption: 'Slide 9'
+  },
+  {
+    src: 'https://photos.zillowstatic.com/cc_ft_1536/ISecymu6nuh4061000000000.webp',
+    // src: '../../assets/EmmaStreet/EmmaStreet00010.webp',
+    altText: 'Slide 10',
+    caption: 'Slide 10'
+  },
+];
+
+export default EmmaStreetImages
--- src/routes/EmmaStreet.js
+++ src/routes/EmmaStreet.js
@@ -1,31 +1,34 @@
-import React from "react"
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {
+  Gallery,
+  GalleryImage
+} from 'react-gesture-gallery'
+import images from '../components/EmmaStreetImages'
+import Layout from '../components/Layout'
 
-import Layout from "../components/Layout"
 
-const EmmaStreet = () => {
-  return(
+function EmmaStreet() {
+  const [index, setIndex] = React.useState(0)
+  return (
     <Layout>
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
+      <Gallery
+        style={{
+          width: '100vw',
+          background: 'black'
+        }}
+        index={index}
+        onRequestChange={i => {
+          setIndex(i)
+        }}
+      >
+        {images.map(image => (
+          <GalleryImage
+            src={image}
+            objectFit="contain"
+          />
+        ))}
+      </Gallery>
     </Layout>
   )
 }
